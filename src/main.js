ca35f801efdae36320b59f2034af34a12286e139
Maicon Silva <maiconrs95@gmail.com>
2020-07-26 00:06:07 -0300

    feat: add commander

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log('Hello World');
+const { Command } = require('commander');
+const pkg = require('../package.json');
+
+const program = new Command();
+
+program
+  .version(pkg.version)
+  .description('A CLI to convert Bitcoin to any currency provided')
+  .parse(process.argv);
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,14 +1,15 @@
 const { expect } = require('chai');
 const { exec } = require('child_process');
+const pkg = require('../package.json');
 
 const btcConverter = './src/main.js';
 
 describe('Main CLI', () => {
-  it('should return hello world', (done) => {
-    exec(btcConverter, (err, stdout, stderr) => {
+  it('should return CLI version', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
       if (err) throw err;
 
-      expect(stdout.replace('\n', '')).to.be.equal('Hello World');
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
       done();
     });
   });
